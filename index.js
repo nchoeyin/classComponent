1a81bf5d037abb74beabd1e848e2d57b9c73d192
Ngawang Choeying <ngawang44@outlook.com>
2022-05-19 22:41:54 +0530

    A frugal web page using components

---
+++ index.js
@@ -0,0 +1,91 @@
+class Top extends React.Component {
+    render() {
+        return (
+            <h1>Web page using component</h1>  
+        );
+    }
+}
+ReactDOM.render(<Top />, document.getElementById('top'));
+class JSXDemo extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/student.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo />, document.getElementById('root'));
+
+class JSXDemo1 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img1.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo1 />, document.getElementById('root1'));
+
+class JSXDemo2 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img2.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo2 />, document.getElementById('root2'));
+
+class JSXDemo3 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img3.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo3 />, document.getElementById('root3'));
+
+class JSXDemo4 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img4.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo4 />, document.getElementById('root4'));
+
+class JSXDemo5 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img5.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo5 />, document.getElementById('root5'));
+
+class JSXDemo6 extends React.Component {
+    render() {
+        return (
+            // <div>
+            // <h5>student studying</h5>
+            <img src="images/img6.jpg" heigth="200" width="400" alt="Image"/>
+            // </div>
+        );
+    }
+}
+ReactDOM.render(<JSXDemo6 />, document.getElementById('root6'));
